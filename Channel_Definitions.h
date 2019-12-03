465799034b7435170466bf0daaf289a54936f85d
mbA2D <ottawaelectronics2016@gmail.com>
2019-12-02 20:37:21 -0500

    Initial commit

---
+++ Channel_Definitions.h
@@ -0,0 +1,180 @@
+//39XXK (XV) is brown wires on positive end
+//33XXK (XH) is red wires on negative end
+
+//PIN CONNECTION DEFINITIONS
+
+#define SPEAKER_PIN 11
+
+#define MAIN_CHANNEL 0
+#define AUX_CHANNEL 1
+
+#define NUM_CHANNELS 64
+
+#define MAIN_CHANNEL_S0 6
+#define MAIN_CHANNEL_S1 5
+#define MAIN_CHANNEL_S2 4
+#define MAIN_CHANNEL_S3 3
+
+#define AUX_CHANNEL_S0 7
+#define AUX_CHANNEL_S1 8
+#define AUX_CHANNEL_S2 9
+#define AUX_CHANNEL_S3 10
+
+#define INPUT_A0 AUX_SIG
+#define INPUT_A1 MAIN1_SIG
+#define INPUT_A2 MAIN2_SIG
+#define INPUT_A3 MAIN3_SIG
+
+
+//VOLTAGE DIVIDER DEFINITIONS
+
+#define UPPER_RESISTOR_VALUE_OHMS 10000
+#define EXCITATION_VOLTAGE_VALUE_VOLTS 5
+
+
+//WHAT IS ON EACH CHANNEL?
+enum devices{
+  NTC_NXRT15XH,
+  NTC_NXRT15XV,
+  V_DIV_2,
+  V_DIV_4,
+  VOLTAGE
+};
+
+/*
+const String channel_names[NUM_CHANNELS] = 
+{
+  ("AUX_0"),
+  ("MAIN0_0"),
+  ("MAIN1_0"),
+  ("MAIN2_0"),
+  ("AUX_1"),
+  ("MAIN0_1"),
+  ("MAIN1_1"),
+  ("MAIN2_1"),
+  ("AUX_2"),
+  ("MAIN0_2"),
+  ("MAIN1_2"),
+  ("MAIN2_2"),
+  ("AUX_3"),
+  ("MAIN0_3"),
+  ("MAIN1_3"),
+  ("MAIN2_3"),
+  ("AUX_4"),
+  ("MAIN0_4"),
+  ("MAIN1_4"),
+  ("MAIN2_4"),
+  ("AUX_5"),
+  ("MAIN0_5"),
+  ("MAIN1_5"),
+  ("MAIN2_5"),
+  ("AUX_6"),
+  ("MAIN0_6"),
+  ("MAIN1_6"),
+  ("MAIN2_6"),
+  ("AUX_7"),
+  ("MAIN0_7"),
+  ("MAIN1_7"),
+  ("MAIN2_7"),
+  ("AUX_8"),
+  ("MAIN0_8"),
+  ("MAIN1_8"),
+  ("MAIN2_8"),
+  ("AUX_9"),
+  ("MAIN0_9"),
+  ("MAIN1_9"),
+  ("MAIN2_9"),
+  ("AUX_10"),
+  ("MAIN0_10"),
+  ("MAIN1_10"),
+  ("MAIN2_10"),
+  ("AUX_11"),
+  ("MAIN0_11"),
+  ("MAIN1_11"),
+  ("MAIN2_11"),
+  ("AUX_12"),
+  ("MAIN0_12"),
+  ("MAIN1_12"),
+  ("MAIN2_12"),
+  ("AUX_13"),
+  ("MAIN0_13"),
+  ("MAIN1_13"),
+  ("MAIN2_13"),
+  ("AUX_14"),
+  ("MAIN0_14"),
+  ("MAIN1_14"),
+  ("MAIN2_14"),
+  ("AUX_15"),
+  ("MAIN0_15"),
+  ("MAIN1_15"),
+  ("MAIN2_15")
+};
+*/
+
+uint8_t channel_connected_device[NUM_CHANNELS] = 
+{
+  V_DIV_2, //AUX 0
+  NTC_NXRT15XV, //MAIN0 0 //3-2-T-P
+  NTC_NXRT15XV, //MAIN1 0 //6-3-B-P
+  NTC_NXRT15XV, //MAIN2 0 //8-1-B-P
+  V_DIV_2, //AUX 1
+  NTC_NXRT15XH, //MAIN0 1 //3-2-B-N
+  NTC_NXRT15XH,//MAIN1 1 //6-3-T-N
+  NTC_NXRT15XH,//MAIN2 1 //8-1-T-N
+  V_DIV_2, //AUX 2
+  NTC_NXRT15XV,//MAIN0 2 //3-3-T-P
+  NTC_NXRT15XV,//MAIN1 2 //5-1-B-P
+  NTC_NXRT15XV,//MAIN2 2 //8-2-B-P
+  V_DIV_2, //AUX 3
+  NTC_NXRT15XH,//MAIN0 3 //3-3-B-N
+  NTC_NXRT15XH,//MAIN1 3 //5-1-T-N
+  NTC_NXRT15XH,//MAIN2 3 //8-2-T-N
+  V_DIV_2, //AUX 4
+  NTC_NXRT15XV,//MAIN0 4 //2-1-T-P
+  NTC_NXRT15XV,//MAIN1 4 //5-2-B-P
+  NTC_NXRT15XV, //MAIN2 4 //8-3-B-P
+  V_DIV_2, //AUX 5
+  NTC_NXRT15XH,//MAIN0 5 //2-1-B-N
+  NTC_NXRT15XH,//MAIN1 5 //5-2-T-N
+  NTC_NXRT15XH,//MAIN2 5 //8-3-T-N
+  V_DIV_2, //AUX 6
+  NTC_NXRT15XV,//MAIN0 6 //2-2-T-P
+  NTC_NXRT15XV,//MAIN1 6 //5-3-T-N
+  NTC_NXRT15XV,//MAIN2 6 //7-1-B-P
+  V_DIV_2, //AUX 7
+  NTC_NXRT15XH,//MAIN0 7 //2-2-B-N
+  NTC_NXRT15XH,//MAIN1 7 //5-3-T-N
+  NTC_NXRT15XH,//MAIN2 7 //7-1-T-N
+  V_DIV_2, //AUX 8
+  NTC_NXRT15XV,//MAIN0 8 //2-3-T-P
+  NTC_NXRT15XV,//MAIN1 8 //4-1-T-P
+  NTC_NXRT15XV,//MAIN2 8 //7-2-B-P
+  V_DIV_2, //AUX 9
+  NTC_NXRT15XH,//MAIN0 9 //2-3-B-N
+  NTC_NXRT15XH,//MAIN1 9 //4-1-B-N
+  NTC_NXRT15XH,//MAIN2 9 //7-2-T-N
+  V_DIV_2, //AUX 10
+  NTC_NXRT15XV,//MAIN0 10 //1-1-T-P
+  NTC_NXRT15XV,//MAIN1 10 //4-2-T-P
+  NTC_NXRT15XV,//MAIN2 10 //7-3-B-P
+  V_DIV_2,//AUX 11
+  NTC_NXRT15XH,//MAIN0 11 //1-1-B-N
+  NTC_NXRT15XH,//MAIN1 11 //4-2-B-N
+  NTC_NXRT15XH,//MAIN2 11 //7-3-T-N
+  V_DIV_2,//AUX 12
+  NTC_NXRT15XV,//MAIN0 12 //1-2-T-P
+  NTC_NXRT15XV,//MAIN1 12 //4-3-T-P
+  NTC_NXRT15XV,//MAIN2 12 //6-1-B-P
+  V_DIV_2,//AUX 13
+  NTC_NXRT15XH,//MAIN0 13 //1-2-B-N
+  NTC_NXRT15XH,//MAIN1 13 //4-3-B-N
+  NTC_NXRT15XH,//MAIN2 13 //6-1-T-N
+  V_DIV_2,//AUX 14
+  NTC_NXRT15XV,//MAIN0 14 //1-3-T-P
+  NTC_NXRT15XV,//MAIN1 14 //3-1-T-P
+  NTC_NXRT15XV,//MAIN2 14 //6-2-B-P
+  V_DIV_2,//AUX 15
+  NTC_NXRT15XH,//MAIN0 15 //1-3-B-N
+  NTC_NXRT15XH,//MAIN1 15 //3-1-B-N
+  NTC_NXRT15XH//MAIN2 15 //6-2-T-N
+};
